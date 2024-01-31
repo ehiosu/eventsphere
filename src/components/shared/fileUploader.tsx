223d1124c8894f4cf7cdf1feb7a49d0b493b06ad
ehiosu <ehiandbrothers@gmail.com>
2024-01-31 01:18:15 +0100

    add upload thing file uploader component

--- src/components/shared/fileUploader.tsx
+++ src/components/shared/fileUploader.tsx
@@ -1,15 +1,36 @@
-import React, { Dispatch, SetStateAction } from 'react'
-type fileUploaderProps={
-    imageUrl:string,
-    onFieldChange:(value:string)=>void,
-    setFiles:Dispatch<SetStateAction<File[]>>
-}
-const FileUploader = ({imageUrl,onFieldChange,setFiles}:fileUploaderProps) => {
-  return (
-    <div>
+import { UploadDropzone } from "@/lib/uploadthing";
+import { useDropzone } from "@uploadthing/react";
+import React, { Dispatch, SetStateAction, useCallback, useState } from "react";
+import { generateClientDropzoneAccept } from "uploadthing/client";
+type fileUploaderProps = {
+    imageSrc:string,
+  onFieldChange: (value: string) => void;
+  setImageSrc: Dispatch<SetStateAction<string>>;
+};
 
-    </div>
-  )
-}
+const FileUploader = ({
+    imageSrc,
+  onFieldChange,    
+    setImageSrc,
+}: fileUploaderProps) => {
+return(
+    <>
+    <UploadDropzone
+    
+    endpoint="imageUploader"
+    onClientUploadComplete={(res) => {
+      // Do something with the response
+      console.log("Files: ", res);
+      onFieldChange(res[0].url)
+      setImageSrc(res[0].url)
+    }}
+    onUploadError={(error: Error) => {
+      // Do something with the error.
+      alert(`ERROR! ${error.message}`);
+    }}
+    
+    /></>
+)
+};
 
-export default FileUploader
+export default FileUploader;
